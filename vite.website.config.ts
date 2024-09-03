dd9d09a8becb947bf60f9c8bdbccb992cb2cf829
Daniil Zagumennii <danielzagumennyi@gmail.com>
2024-09-03 17:57:38 +0300

    update

--- vite.website.config.ts
+++ vite.website.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/draggable-ui",
   plugins: [react()],
   build: {
     outDir: "docs",
